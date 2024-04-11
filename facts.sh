8a0dfa8857706fd6c3a46d38ef61e2d003bf603a
Keegan Campbell <me@kfcampbell.com>
2024-04-11 12:55:33 -0700

    Added fact about UW PAC-12 game (#6)

--- facts.sh
+++ facts.sh
@@ -7,6 +7,7 @@ facts=(
 	"The state tree of Washington is the Western Hemlock."
 	"All mammals get goosebumps."
 	"Queen Alexandra's Birdwing is the largest butterfly in the world: females can reach wingspans of 28cm (11 inches)."
+	"The University of Washington won the last PAC-12 football championship in 2023."
 )
 
 # array to keep track of which facts have been viewed
